2cd5b5b4cafec6f104436f1b2dc1e323f32cd290
osullif7 <osullif7@tcd.ie>
2023-11-28 18:56:53 +0100

    cleaned up functions

--- src/main/java/be/kuleuven/distributedsystems/cloud/controller/EuroStarInitController.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/controller/EuroStarInitController.java
@@ -1,12 +1,16 @@
 package be.kuleuven.distributedsystems.cloud.controller;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.FileReader;
+import java.util.concurrent.ExecutionException;
 
 public class EuroStarInitController {
     private final Firestore firestore;
@@ -14,11 +18,25 @@ public class EuroStarInitController {
     @Autowired
     public EuroStarInitController(Firestore firestore) { this.firestore = firestore; }
 
-    public void addTrainInfo() {
+    private void addTrainInfo() {
         DocumentReference docRef = firestore.collection("OurTrain").document();
 
+        if (!dataInitialised()) {
+
+            //add data
+        }
 
     }
 
+    private boolean dataInitialised() {
+        CollectionReference collectionReference = firestore.collection("OurTrain");
+        ApiFuture<DocumentSnapshot> future = collectionReference.document("train").get();
+
+        try {
+            return !future.get().exists();
+        } catch (ExecutionException | InterruptedException e) {
+            return true;
+        }
+    }
 
 }
