e3ae43276211cb975acd061d1b4d21b897b2d714
osullif7 <osullif7@tcd.ie>
2023-11-30 22:55:51 +0100

    comments and cleaning up

--- src/main/java/be/kuleuven/distributedsystems/cloud/PubSub/TicketsTopic.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/PubSub/TicketsTopic.java
@@ -1,11 +1,10 @@
-package be.kuleuven.distributedsystems.cloud.PubSUb;
+package be.kuleuven.distributedsystems.cloud.PubSub;
 
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.NoCredentialsProvider;
 import com.google.api.gax.grpc.GrpcTransportChannel;
 import com.google.api.gax.rpc.FixedTransportChannelProvider;
 import com.google.api.gax.rpc.TransportChannelProvider;
-import com.google.cloud.pubsub.v1.Publisher;
 import com.google.cloud.pubsub.v1.TopicAdminClient;
 import com.google.cloud.pubsub.v1.TopicAdminSettings;
 import com.google.pubsub.v1.Topic;
